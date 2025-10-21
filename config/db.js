eddd4319ec828837e4f147eed7dac8667c6658d0
Abhishek <abhishekak87378@gmail.com>
2025-10-20 19:45:17 -1200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect("mongodb+srv://abhishek4712ak_db_user:TZbpuE6htfJYVZ6J@zest.k8zstju.mongodb.net/?retryWrites=true&w=majority&appName=zest");
         console.log("database connected successfully")
     }catch(error){
         console.log("error in db.js", error)
