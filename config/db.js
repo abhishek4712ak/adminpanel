059c896b7567c77abec46d4eb728116cf5610109
Abhishek <abhishekak87378@gmail.com>
2025-12-23 23:04:38 -1200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async()=>{
     try{
-        await mongoose.connect(process.env.DB_URL);
+        await mongoose.connect("mongodb+srv://abhishek4712ak1_db_user:2mBJMySRZVGT9ohw@cluster0.t5tthny.mongodb.net/ADMIN?appName=Cluster0");
         console.log("database connected successfully")
     }catch(error){
         console.log("error in db.js", error)
